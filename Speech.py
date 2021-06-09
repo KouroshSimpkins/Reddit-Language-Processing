b01cee91ace8f89b118bb191e8b397e2750c399b
Kourosh Simpkins <k.simpkins0820201@arts.ac.uk>
2021-06-09 17:01:31 +0100

    Finalised Output
    Final output with the file that generated the audio file.

--- Speech.py
+++ Speech.py
@@ -1,7 +1,20 @@
 import pyttsx3
+import sqlite3
+
+conn = sqlite3.connect('reddit_data.db')
+cursor = conn.cursor()
+
+def Read_From_db():
+    Speech = []
+    cursor.execute('SELECT body FROM reddit_comments')
+    data = cursor.fetchall()
+    print(data)
+    for line in data:
+        Speech.append(line)
+    return(Speech)
+
 
 engine = pyttsx3.init()
-engine.say("I will speak this text now")
-engine.save_to_file("I will speak this text now", "test.mp3")
+engine.save_to_file(str(Read_From_db()), 'output.mp3')
 engine.runAndWait()
 engine.stop()
